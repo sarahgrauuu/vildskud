fc94708e3b6fc156f6c3a01573b4adfc6ce8572b
mgcviii <111751220+mgcviii@users.noreply.github.com>
2023-03-20 11:50:52 +0100

    added the colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      yellow: "#FDE76F",
+      blue: "#3468DB",
+      red: "#CE0E17",
+      orange: "#FFA92C",
+      green: "#4FB47D",
+      black: "#212121",
+      white: "#F3F3F3"
+
+
+    }
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
