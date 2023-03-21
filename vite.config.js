bd4ce080f8af6744a3a2607d085246b7516e454a
Sarah <sarah.G2018@outlook.com>
2023-03-21 19:32:17 +0100

    let me check

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,9 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         event1: resolve(__dirname, "src/event1.html"),
         event2: resolve(__dirname, "src/event2.html"),
-<<<<<<< HEAD
         event3: resolve(__dirname, "src/event3.html"),
         opencall: resolve(__dirname, "src/opencall.html"),
-=======
         program: resolve(__dirname, "src/program.html"),
->>>>>>> 6930dffe56b706faf1f9824f558a49ecec295ba8
       },
     },
   },
